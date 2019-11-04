2f4b7600036a3467e11d2b35f9345f4e5366743d
Ismail <ismail.zizi@sciencespo.fr>
2019-11-04 16:09:45 +0100

    Text cleaning (first commands)

--- text_cleaning.py
+++ text_cleaning.py
@@ -1,1 +1,16 @@
-# Sccipt for text cleaning
+# Script for text cleaning
+
+
+# Get tweets
+df1['text']
+
+# Word count
+df1['word_count'] = df1['text'].apply(lambda x : len(str(x).split(" ")))
+
+df1[['text','word_count']]
+
+# Word Count Description
+df1[['text', 'word_count']].describe()
+
+# One-word tweets (usually links or tags)
+df1[['text', 'word_count']][df1['word_count'] == 1].count()
