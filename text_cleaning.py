f0ad61b643258aa24f4df92728ab65dcf5eca0ad
Ismail <ismail.zizi@sciencespo.fr>
2019-11-05 01:59:52 +0100

    Text cleaning (ready for proof-reading)

--- text_cleaning.py
+++ text_cleaning.py
@@ -1,16 +1,106 @@
-# Script for text cleaning
+# Import libraries
 
 
-# Get tweets
+import pandas as pd
+import numpy as np
+import json
+import yfinance as yf
+import nltk
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.wordnet import WordNetLemmatizer
+
+
+
+def get_tweet_data():
+    # Download and save tweet data
+
+    with open('tweets.json', 'rb') as f:
+        data = json.load(f)
+
+    df = pd.DataFrame(data)
+    df.to_excel('tweets.xls', index=False)
+    return df
+
+
+df1 = get_tweet_data()
+
+df1
+
 df1['text']
 
-# Word count
-df1['word_count'] = df1['text'].apply(lambda x : len(str(x).split(" ")))
 
-df1[['text','word_count']]
+# Add word_count column
 
-# Word Count Description
+df1['word_count'] = df1['text'].apply(lambda x: len(str(x).split(" ")))
+
+df1[['text', 'word_count']]
+
+
+# Word count statistical description
+
 df1[['text', 'word_count']].describe()
 
-# One-word tweets (usually links or tags)
+
+# 1-word tweets, usually hhtp links, # tags or @ tags)
+
+df1[['text', 'word_count']][df1['word_count'] == 1].head()
 df1[['text', 'word_count']][df1['word_count'] == 1].count()
+
+
+# 20 most common words
+
+freq_common = pd.Series(' '.join(df1['text']).split()).value_counts()[:20]
+freq_common
+
+
+# 20 less common words
+
+freq_uncommon = pd.Series(' '.join(df1['text']).split()).value_counts()[-20:]
+freq_uncommon
+
+
+# Remove 'RT + source account' from tweets
+
+retweets = df1["text"][df1['is_retweet'] == True]
+removeRT = retweets.str[3:]
+cleanretweets = removeRT.str.split(n=1).str[1]
+df1["text"][df1['is_retweet'] == True] = cleanretweets
+df1["text"][df1['is_retweet'] == True]
+
+
+# Setting frequent words without contextual meaning, 'stop_words' is built-in, 'new_words' is chosen.
+
+stop_words = set(stopwords.words('english'))
+new_words = ["using", "show", "result", "large", "also", "iv", "one", "two", "new", "previously", "shown"]
+stop_words = stop_words.union(new_words)
+
+
+# Removing links from tweets
+
+df1["cleantext1"] = df1['text'].str.replace(
+    'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', "")
+df1["cleantext1"]
+
+
+# Remove punctuation
+
+df1["cleantext2"] = df1["cleantext1"].str.replace('[^\w\s]', '')
+df1["cleantext2"]
+
+
+# Remove special characters and digits
+
+df1["cleantext3"] = df1["cleantext2"].str.replace("(\\d|\\W)+", " ")
+df1["cleantext3"]
+
+
+# Put all words in lower case
+
+df1["cleantext4"] = df1["cleantext3"].str.lower()
+df1["cleantext4"]
+
+
